3e6ad5992fa9f828157b3074711cc000ee181d6e
Viktor Voronin <mobimrzg@gmail.com>
2018-03-13 17:01:45 +0300

    Finish 2 door

--- js/door2.js
+++ js/door2.js
@@ -6,16 +6,20 @@
  */
 function Door2(number, onUnlock) {
   DoorBase.apply(this, arguments);
-
-  // ==== Напишите свой код для открытия третей двери здесь ====
-  // Для примера дверь откроется просто по клику на неё
-  this.popup.addEventListener(
-    "click",
-    function() {
-      this.unlock();
-    }.bind(this)
-  );
-  // ==== END Напишите свой код для открытия третей двери здесь ====
+  const onUnlockButtonPointerDown = () => {
+    this.unlock();
+  };
+  const zoomarea = new Zoom(".popup_level_2__zoomarea");
+  const button = this.popup.querySelector(".door-riddle__button_6");
+  button.addEventListener("pointerdown", onUnlockButtonPointerDown.bind(this));
+  zoomarea.addListener((currDistance, initialDistance) => {
+    const diff = currDistance - initialDistance;
+    const scale = diff > 0 ? diff * 0.01 : 0;
+    button.style.transform = `translateX(-50%) scale(${scale},${scale}`;
+  });
+  zoomarea.addReleaseListener(() => {
+    button.setAttribute("style", "transform: translateX(-50%) scale(0,0)");
+  });
 }
 Door2.prototype = Object.create(DoorBase.prototype);
 Door2.prototype.constructor = DoorBase;
